9ef08f94c2bffe6416afed89842165b86d6dbba7
Gabriel <gabriel@gabriel-System-Product-Name>
2024-01-08 16:23:28 -0800

    wrote test for insertAtEnd funciton

--- test/LinkedListTest.cpp
+++ test/LinkedListTest.cpp
@@ -16,3 +16,25 @@ TEST(LinkedListTest, insertAtbeginning)
 
     EXPECT_EQ(list.getHeadvalue(), 10);
 }
+
+/**
+ * @brief test for the insert at end function
+ * in the linked list class
+ * 
+ */
+TEST(LinkedListTest, insertAtEnd)
+{
+    LinkedList list;
+    for (int i = 0; i <= 10; i++)
+    {
+        list.insertAtEnd(i);
+    }
+
+    Node<int>* temp = list.getHead();
+    while(temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+
+    EXPECT_EQ(temp->data, 10);
+}
