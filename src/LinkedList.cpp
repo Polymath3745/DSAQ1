168031a0afbf042e5877a9b59e5b038e49b6f4e0
Gabriel <gabriel@gabriel-System-Product-Name>
2024-01-08 16:21:12 -0800

    implemented the insertAtEnd function

--- src/LinkedList.cpp
+++ src/LinkedList.cpp
@@ -19,3 +19,21 @@ void LinkedList::insertAtbeggining(int val)
     newNode->next = head;
     head = newNode;
 }
+
+void LinkedList::insertAtEnd(int val)
+{
+    Node<int>* newNode = new Node<int>(val);
+    if(head == nullptr)
+    {
+        head = newNode;
+        return;
+    }
+
+    Node<int>* currentNode = head;
+    while(currentNode->next != nullptr)
+    {
+        currentNode = currentNode->next;
+    }
+
+    currentNode->next = newNode;
+}
