ac366cef840d82133dfb2c7a82c640d2e03e9f76
Gabriel <gabriel@gabriel-System-Product-Name>
2024-01-09 19:25:46 -0800

    implemented general insert function

--- src/LinkedList.cpp
+++ src/LinkedList.cpp
@@ -37,3 +37,37 @@ void LinkedList::insertAtEnd(int val)
 
     currentNode->next = newNode;
 }
+
+void LinkedList::insert(int val, int target)
+{
+    Node<int>* newNode = new Node<int>(val);
+    if(head == nullptr)
+    {
+        head = newNode;
+        return;
+    }
+
+    int idxcnt = 0;
+    Node<int>* currentNode = head;
+    Node<int>* prevNode = nullptr;
+    while(currentNode->next != nullptr && currentNode->data != target)
+    {
+        prevNode = currentNode;
+        currentNode = currentNode->next;
+    }
+
+    if (currentNode->data != target)
+    {
+        std::cout << "target node not found" << std::endl;
+        return;
+    }
+
+    if(currentNode->next == nullptr && currentNode->data == target)
+    {
+        prevNode->next = newNode;
+        newNode->next = currentNode;
+    }
+
+    prevNode->next = newNode;
+    newNode->next = currentNode;
+}
